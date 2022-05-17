49cc7b17cf77ba2de7011f9f8858a35ca23e7f7c
Dowon Oh <5d1@hits.ai>
2022-05-17 23:36:29 +0900

    body 파싱을 위한 express.json() 미들웨어 등록

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const PORT = 5000;
 
 const app = express();
+app.use(express.json); // 요청바디를 파싱하기 위해 내장 BodyParser를 사용
+
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
